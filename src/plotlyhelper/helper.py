fbf9d2eaf2247cd800a6e782505089819163c365
TILLET Nans p2003292 <nans.tillet@etu.univ-lyon1.fr>
2025-08-22 21:12:28 +0200

    [Update] plotly graph and remove unused file

--- src/plotlyhelper/helper.py
+++ src/plotlyhelper/helper.py
@@ -1,29 +1,65 @@
 from typing import List
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.colors as pc
+
+from analyzer.analyzer import AnalyseOutOfBoundFullProperty
+from .analyzer_helper import displayAppearanceMapping
 from base.propertyClass import FullProperty, ValueProperty
 from base.tool import PropertyModifier, easyLerp, transformValue
+
+
 
 def DisplayLineWithSimpleLerp(x : List[float], y : List[float], fullName : str, lineName : str) -> None :
     assert len(x) == len(y), f"Error in DisplayLineWithSimpleLerp. Not all list are the same length: {len(x)} != {len(y)}."
     x = x.copy()
     y = y.copy()
-    
-    color : List[str] = []
-    [color.append(lineName) for _ in x]
 
-    
-    color.append("linear")
-    x.append(x[0])
-    y.append(y[0])
+    colorMap : dict[str, str] = {
+        lineName        : "blue",
+        "linear"        : "red",
+        "bound_begin"   : "green",
+        "bound_end"     : "orange"
+    }
 
-    color.append("linear")
-    x.append(x[-2])
-    y.append(y[-2])
+    lineTypeMap : dict[str, str] = {
+        lineName        : "solid",
+        "linear"        : "dashdot",
+        "bound_begin"   : "dot",
+        "bound_end"     : "dot"
+    }
     
+    lineType : List[str] = []
+    [lineType.append(lineName) for _ in x]
 
-    fig = px.line(x=x, y=y, title=fullName, color=color)
+    minVal = x[0],  y[0]
+    maxVal = x[-1], y[-1]
+
+    def append(colorName : str, xValue : float, yValue : float) :
+        lineType.append(colorName)
+        x.append(xValue)
+        y.append(yValue)
+
+    append("linear",        minVal[0], minVal[1])
+    append("linear",        maxVal[0], maxVal[1])
+
+    append("bound_begin",   minVal[0], minVal[1])
+    append("bound_begin",   maxVal[0], minVal[1])
+
+    append("bound_end",     minVal[0], maxVal[1])
+    append("bound_end",     maxVal[0], maxVal[1])
+
+
+    df = pd.DataFrame({"x": x, "y": y, "lineType": lineType})
+
+    fig = px.line(
+        df, x="x", y="y", color="lineType", line_dash="lineType",
+        color_discrete_map=colorMap,
+        line_dash_map=lineTypeMap,
+        title=fullName
+    )
+
     fig.show()
 
 
@@ -37,7 +73,7 @@ def DisplayPropertyModifier(x : List[float], propertymodifier : PropertyModifier
 
 def DisplayValuePropertyLerp(x : List[float], property0 : ValueProperty, property1 : ValueProperty, name : str) -> None :
     lerped : List[ValueProperty] = easyLerp(property0, property1, x)
-    realValues =  ValueProperty.transformValues(lerped)
+    realValues : List[float] =  ValueProperty.transformValues(lerped)
     DisplayLineWithSimpleLerp(x, realValues, f"graph of lerp of transfomed value from value property:  {name}", name)
 
 
@@ -62,3 +98,6 @@ def MakeFullPropertyExample(iterator : List[float], property0 : FullProperty, pr
 
     DisplayValuePropertyLerp(iterator, property0.propertyX, property1.propertyX, "propertyX")
     DisplayValuePropertyLerp(iterator, property0.propertyY, property1.propertyY, "propertyY")
+
+    mapping : dict[tuple[int, int], int] = AnalyseOutOfBoundFullProperty(lerped, property0, property1)
+    displayAppearanceMapping(mapping, "example")
