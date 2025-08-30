4070b99c4e08676b45a3f34a132aca9e174f4679
TILLET Nans p2003292 <nans.tillet@etu.univ-lyon1.fr>
2025-08-30 18:17:50 +0200

    [New] Power property

--- src/modifier/functionProperty.py
+++ src/modifier/functionProperty.py
@@ -78,31 +78,62 @@ class FunctionPropertyMix(PropertyModifier):
     
     
 
-class FunctionProperty(PropertyModifier):
+
+class FunctionProperty():
+    def transformPositiveValue(self, baseValue : float) -> float :
+        ...
+
+    def transformValue(self, baseValue : float) -> float :
+        sign = math.copysign(1, baseValue)
+        result = self.transformPositiveValue(abs(baseValue))
+        return result * sign
+
+    def quickPrint(self) :
+        ...
+
+    def __call__(self, value : float) -> float :
+        return self.transformValue(value)
+
+class FunctionPropertyPow(FunctionProperty):
+    def __init__(self, power : float, x_1 : float, y_1 : float) :
+        assert x_1 > 0, "Error in FunctionPropertyPow. x_1 must be over 0."
+        assert y_1 > 0, "Error in FunctionPropertyPow. y_1 must be over 0."
+        assert power > 0, "Error in FunctionPropertyPow. power must be over 0."
+
+        self.power : float = power
+        self.x_1 : float = x_1
+        self.y_1 : float = y_1
+
+    def transformPositiveValue(self, baseValue : float) -> float :
+        value = baseValue / self.x_1
+        value = pow(value, self.power)
+        value = value * self.y_1
+        return value
+    
+    def quickPrint(self) :
+        print(f"power: {self.power}\tx_1: {self.x_1}\ty_1: {self.y_1}")
+
+    @staticmethod
+    def rndCenter1(scale : float) -> float :
+        tmp = random.uniform(-scale, scale)
+        if tmp >= 0 : return tmp + 1 
+        else : return 1 / (1 - tmp)
+
+    @staticmethod
+    def rnd(scale : float) -> "FunctionPropertyPow" :
+        return FunctionPropertyPow(
+            math.sqrt(math.sqrt(FunctionPropertyPow.rndCenter1(scale))),
+            math.sqrt(FunctionPropertyPow.rndCenter1(scale)),
+            math.sqrt(FunctionPropertyPow.rndCenter1(scale)),
+        ) 
+
+
+class FunctionPropertyFunc(FunctionProperty):
     def __init__(self, function : Callable[[float], float]) :
         self.func : Callable[[float], float] = function
 
-    def transformValue(self, baseValue : float) -> float :
+    def transformPositiveValue(self, baseValue : float) -> float :
         return self.func(baseValue)
     
     def quickPrint(self) :
         print()
-
-
-    def __add__(self, other) :
-        return FunctionPropertyMix.valueToFunctionPropertyMix(self) + FunctionPropertyMix.valueToFunctionPropertyMix(other)
-    
-    def __mul__(self, weight : float) :
-        return FunctionPropertyMix.valueToFunctionPropertyMix(self) * weight
-    
-    def __call__(self, value : float) -> float :
-        return self.transformValue(value)
-
-
-    @staticmethod
-    def lerp(val0 : ClassLerp, val1 : ClassLerp, coef : float) -> ClassLerp :
-        return lerpToWeightedMean(val0, val1, coef, FunctionProperty.weightedMean)
-    
-    @staticmethod
-    def weightedMean(values : Sequence[ClassLerp], weights : List[float]) -> ClassLerp :
-        return FunctionPropertyMix.weightedMean(values, weights)
